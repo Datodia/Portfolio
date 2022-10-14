51c910a438b55b16f0262de88aa58df01146ed71
Datodia <dato.diasamidze.02@gmail.com>
2022-10-14 16:35:00 +0400

    change logo

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ const form = document.querySelector('form');
 const btn = document.querySelector('.test');
 const closeBtn = document.querySelector('#close');
 const main = document.querySelector('.main-container')
+const logo = document.querySelector('#logo')
+const logo2 = document.querySelector('#logo2')
+
+
+logo.textContent = "<DD />"
+logo2.textContent = "<DD />"
+
 btn.addEventListener('click', () => {
     form.style.display = 'flex'
     // main.style.filter = 'blur(.2rem)'
